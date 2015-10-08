d9ca3b059439d7b8a392508e657389a265494929
spialelo <ndphillips@gmail.com>
2015-10-07 22:21:48 -0400

    Adding to the bag and then showing what's in the bag. Check.

--- js/myConcept.js
+++ js/myConcept.js
@@ -9,18 +9,20 @@
     var makeupBag = [];
 
 
-    function Makeup(brand, name, type, expDate){
+    function Makeup(brand, mkname, type, expDate){
 
         this.brand = brand;
-        this.name = name;
+        this.mkname = mkname;
         this.type = type;
         this.expDate = expDate;
         this.makeupToString = function(){
-          return "You have " + this.brand + " " + this.name+ " in your makeup bag. It expires on " + this.expDate + ".";
+          return "You have "+this.brand + " "+this.mkname+ " in your makeup bag. It has an expiration date of "+this.expDate+". It is a "+this.type+".";
         };
 
     }
 
+
+    /*adding things to the bag */
 
     function addToBag(){
 
@@ -30,39 +32,75 @@
         var inputExpDate = document.getElementById('mkExpDate').value;
 
 
-    if (inputBrand != "" && inputName != "" && inputType != "" && inputExpDate != "") {
+        if(inputBrand != "" && inputName != "" && inputType != "" && inputExpDate != "") {
 
             var product_1 = new Makeup(inputBrand,inputName,inputType,inputExpDate);
+            makeupBag.push(product_1);
+            document.getElementById('feedbackMakeup').textContent = makeupBag.length + " item(s) in your makeup bag.";
 
-            console.log(product_1.brand + ", " + product_1.name);
-
-            document.getElementById('displayBag').textContent = product_1.makeupToString();
+        /*console.log(product_1.brand + ", " + product_1.name);
+        document.getElementById('displayBag').textContent = product_1.makeupToString();*/
 
         }
 
-
-        else if (inputBrand == "" || inputName == "" || inputType == "" || inputExpDate == ""){
+        else if(inputBrand == "" || inputName == "" || inputType == "" || inputExpDate == ""){
 
             document.getElementById('displayBag').textContent = "Add something to your make up bag. :)";
 
         }
 
 
-    }
-
-
-    function eraseScreen (){
-
-        document.getElementById('displayBag').textContent = "";
-        makeupForm.reset();
+        document.getElementById('mkBrand').value = "";
+        document.getElementById('mkName').value = "";
+        document.getElementById('mkType').value = "";
+        document.getElementById('mkExpDate').value = "";
 
     }
 
 
-    var buttonMakeup = document.getElementById('makeupButton');
-    buttonMakeup.onclick = addToBag;
+    /* display what is in the bag */
+
+    function showBag (){
+      for(var i = 0; i < makeupBag.length; i++){
+          document.getElementById('displayBag').innerHTML += makeupBag[i].brand + " " +makeupBag[i].mkname +"<br>";
+      }
+  }
+
+
+    var buttonAddMakeup = document.getElementById('addMakeupButton');
+    buttonAddMakeup.onclick = addToBag;
+
+
+    var displayMakeupBag = document.getElementById('showMakeupButton');
+    displayMakeupBag.onclick = showBag;
+
+
+    /*starting all over */
+
+    function eraseDiv() {
+
+
+        var displayField = document.getElementById('displayBag');
+        var feedbackField = document.getElementById('feedbackMakeup');
+        /*Reset the display area to an empty square.*/
+        displayField.textContent = "";
+        feedbackField.textContent = "";
+
+
+        var form = document.getElementById('makeupForm');
+
+        /*Reset form values.*/
+        form.reset();
+
+        /*Reset the display area size to the original values.*/
+        displayField.style.width = "400px";
+        displayField.style.height = "400px";
+
+    }
 
 
 
+    var resetButton = document.getElementById('resetButton');
+    resetButton.onclick = eraseDiv;
 
 })();
